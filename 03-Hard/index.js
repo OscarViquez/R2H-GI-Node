31b76c6cdad200ee4721e6bd5cbaee0cb83bfc3e
Oscar H.V <55415043+OscarViquez@users.noreply.github.com>
2022-06-23 00:01:37 -0400

    =

--- 03-Hard/index.js
+++ 03-Hard/index.js
@@ -1,8 +1,20 @@
-var express = require('express');
-var app = express()
+const express = require('express');
+const app = express();
+const data = require('./employee.json')
+const port = 8001;
 
 // GET METHOD
-app.get()
+app.get('/', (req, res) => {
+    // res.send(JSON.parse(JSON.stringify(data)))
+    res.send(data);
 
+})
+
+app.get('/employee/:employeeID', (req, res) => {
+    res.send(data.Employees[req.params.employeeID - 1])
+})
 
 // PORTS
+app.listen(port, () => {
+    console.log(`app running on ${port}`)
+})
